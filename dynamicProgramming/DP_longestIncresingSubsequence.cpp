d0d17600460ce42ccce0ed640bd361d5207d68e5
VA-run23 <1523codes@gmail.com>
2025-01-30 20:40:00 +0530

    29 jan learnings

---
+++ dynamicProgramming/DP_longestIncresingSubsequence.cpp
@@ -0,0 +1,29 @@
+//36.8 
+//Memoisation
+#include <bits/stdc++.h>
+using namespace std;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define pii pair<int, int>
+#define vii vector<pii>
+#define vpi vector<pii>
+#define rep(i,a,b) for (int i=a; i<b; i++)
+#define ff first
+#define ss second 
+#define setBits(x) __builtin_popcount(x)//counts the number of set bits (1s) in the binary representation of x
+
+
+const int N = 1e5+2, MOD = 1e9+7;
+
+
+int main(){
+    int n;
+    cin>> n;
+    vi a(n);
+    rep(i, 0, n){
+        cin>> a[i];
+    }
+
+    return 0;
+}
